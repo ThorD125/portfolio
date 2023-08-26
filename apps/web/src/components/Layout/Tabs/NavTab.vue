f327e47b4e19ce2e1974e5aae394b9b50203408f
ther <thor.demeestere@telenet.be>
2023-08-26 15:52:16 +0200

    f

---
+++ apps/web/src/components/Layout/Tabs/NavTab.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button class="w-full h-fit" @click="emit('settab')">
+        <slot></slot>    
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface IProps {
+}
+const props = withDefaults(defineProps<IProps>(), {
+});
+
+const emit = defineEmits(['settab']);
+
+</script>
+
+<style scoped></style>
---
+++ apps/web/src/components/Layout/Tabs/TabContent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        tab pane
+        {{ title }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    interface IProps {
+        title: string;
+        show: boolean;
+    }
+    const props = withDefaults(defineProps<IProps>(), {
+        title: 'No title provided',
+        show: false,
+    });
+</script>
+
+<style scoped>
+
+</style>
