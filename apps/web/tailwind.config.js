be3116f67a2f963e1bcfd822e31a8a5ea6f01fff
ther <thor.demeestere@telenet.be>
2023-09-01 23:13:56 +0200

    f

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,9 +6,6 @@ const plugin = require('tailwindcss/plugin');
 module.exports = {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        fontFamily: {
-            sans: ['"PT Sans"', 'sans-serif'],
-        },
         extend: {
             screens: {
                 landscape: {
