7b11f5796315ba285205f1471bd52572ec42c233
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2023-09-12 10:24:22 +0000

    f

--- apps/web/src/components/Layout/Pages/cv.vue
+++ apps/web/src/components/Layout/Pages/cv.vue
@@ -3,6 +3,16 @@
         <div>
             experiences
             <div>
+                Jimber
+                juli-augustus 2023
+                Ik werkte in het begin aan de stijl en later aan nieuwe features van hun network isolation product.
+            </div>
+            <div>
+                Telivic
+                juli-augustus 2022
+                Ze hadden data van klanten in een database systeem die al 3 of vier keer werd samengevoegt met andere verkregen data, waardoor deze er soms 4 keer in zaten onder een andere naam of met oudere emails, mijn taak was dan om deze op te ruimen, hiernaast werkte ik ook nog aan hun crm om kleine dingen aan te passen om voor hen het proces van opdrachten of invoices sneller en gemakkelijker in te kunnen geven
+            </div>
+            <div>
                 Otomaties - Tom Broucke
                 maart 2021
                 https://tombroucke.be/
@@ -12,10 +22,15 @@
         <div>
             education
             <div>
-                Industriele Wetenschappen
-                2012-2015
-                Vti Menen
-                Hier zagen we veel wiskunde.
+                Bachelor toegepaste informatica - Cyber Security Professional
+                2021-...
+                Howest Brugge
+                Opleiding tot een innoverende en veelzijdige informaticus met specialisatie full stack-web ontwikkeling, secure software development, web & mobile native apps, blockchain architectuur en development, cybercrime, web- en netwerk pentesten, forensische analyse, malware analyse en reverse engineering. Kennismaking met Big Data, IoT en IT-wetgeving (GDPR). Kortom een stevige basis IT. Eerste jaar hiervan afgewerkt. In september start ik mijn tweede jaar met als keuzetraject Cyber Security Professional: waar ik vooral cybersecurity voor zal organiseren en leren afrekenen met cybercrime. Geslaagd voor het eerste opleidingsjaar.
+            </div>
+            <div>
+                Tevens behaalde ik ook een stiptheid, orde, discipline, attitude-attest
+                SODA-attest behaald op 29 juni 2021
+                https://sodaplus.be/nl-be
             </div>
             <div>
                 Multimedia - Optie Visueel
@@ -26,15 +41,25 @@
                 Diploma behaald op 29 juni 2021
             </div>
             <div>
-                Tevens behaalde ik ook een stiptheid, orde, discipline, attitude-attest
-                SODA-attest behaald op 29 juni 2021
-                https://sodaplus.be/nl-be
+                Industriele Wetenschappen
+                2012-2015
+                Vti Menen
+                Hier zagen we veel wiskunde.
+            </div>
+        </div>
+        <div>
+            plus points
+            <div>
+                harde werker (aka workaholic)
+                ik werk liever savonds een uurtje verder dan een nieuwe function of iets dergelijks te laten liggen tot de dag erna
             </div>
             <div>
-                Bachelor toegepaste informatica - Cyber Security Professional
-                2021-...
-                Howest Brugge
-                Opleiding tot een innoverende en veelzijdige informaticus met specialisatie full stack-web ontwikkeling, secure software development, web & mobile native apps, blockchain architectuur en development, cybercrime, web- en netwerk pentesten, forensische analyse, malware analyse en reverse engineering. Kennismaking met Big Data, IoT en IT-wetgeving (GDPR). Kortom een stevige basis IT. Eerste jaar hiervan afgewerkt. In september start ik mijn tweede jaar met als keuzetraject Cyber Security Professional: waar ik vooral cybersecurity voor zal organiseren en leren afrekenen met cybercrime. Geslaagd voor het eerste opleidingsjaar.
+                leergierig
+                als er iets die beter kan en ik zie dat niet zou ik liever hebben dat iemand me dit direct zegt dan dat ik dit achter men rug moet horen
+            </div>
+            <div>
+                relaxed
+                als ik zeg dat iets af zal zijn zal ik zo veel mogelijk doen zodat dit als er geen externe factoren dit tegenhouden dit ook af zal zijn en dat u op 2 geruste oren kunst slapen
             </div>
         </div>
     </div>
