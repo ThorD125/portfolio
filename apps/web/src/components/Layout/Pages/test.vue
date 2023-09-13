c489ac2a1dc3e83ac54a450fc63adc0aa6e823cd
Ther <thor.demeestere@telenet.be>
2023-09-14 01:12:08 +0200

    f

--- apps/web/src/components/Layout/Pages/test.vue
+++ apps/web/src/components/Layout/Pages/test.vue
@@ -7,14 +7,14 @@
         <Tile>six</Tile>
         <Tile :bg-image="TestImage" url="example.com">eleven</Tile>
     </TileGrid>
-    <TableGrid :headers="tableheaders">
+    <!-- <TableGrid :headers="tableheaders">
         <TRow><TCol>hackypi</TCol><TCol><a href="https://shop.sb-components.co.uk/products/hackypi-compact-diy-usb-hacking-tool">hackypi-compact-diy-usb-hacking-tool</a></TCol></TRow>
         <TRow><TCol>Music punch bag</TCol><TCol><a href="https://www.amazon.com/ONEPUNCH-Machine-Electronic-Punching-Exercise/dp/B0BYF2QV1L">B0BYF2QV1L</a></TCol></TRow>
         <TRow><TCol>Victrix pro controller</TCol><TCol><a href="https://pdp.com/products/ps5-ps4-pc-victrix-pro-bfg-wireless-controller-las-almas-le">ps5-ps4-pc-victrix-pro-bfg-wireless-controller-las-almas-le</a></TCol></TRow>
         <TRow><TCol>Flipper zero</TCol><TCol><a href="https://lab401.com/products/flipper-zero?variant=42927883485414">flipper-zero</a></TCol></TRow>
         <TRow><TCol>Usb c dock</TCol><TCol><a href="https://www.startech.com/en-be/universal-laptop-docking-stations/dk30ch2depue">dk30ch2depue</a></TCol></TRow>
         <TRow><TCol>Deskboard</TCol><TCol><a href="https://deskboardbuddy.com/pages/deskboard-buddy?utm_content=111271053987&gadid=461911847402">deskboard-buddy</a></TCol></TRow>
-    </TableGrid>
+    </TableGrid> -->
 </template>
 
 <script setup lang="ts">
