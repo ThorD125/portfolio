62d388c8acc31932b472e6621a7276d93ad94136
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2023-10-19 14:30:44 +0200

    Create BlogCloudCafe.vue

---
+++ apps/web/src/components/Layout/Pages/blog/BlogCloudCafe.vue
@@ -0,0 +1,15 @@
+
+Wat een geweldige ervaring was het bij het Campus Cloud Café in Howest, Kortrijk! Het evenement begon om 17.00 uur en beloofde een boeiende dag vol waardevolle inzichten en kansen om ons professionele netwerk uit te breiden.
+
+De eerste sessie, "Real-life Gebruikstoepassingen met Azure OpenAI" gepresenteerd door Mathias Cheyns, was echt inspirerend. Hij nam ons mee op een reis door de toepassingen van Azure OpenAI in verschillende industrieën en liet zien hoe deze geavanceerde AI-tools echte problemen kunnen oplossen. Als ontwikkelaar of IT-professional was ik zeker geïnspireerd door de mogelijkheden van kunstmatige intelligentie.
+
+De tweede sessie, "Lessen geleerd bij het bouwen van een Industrieel IoT-platform op Azure" door Toon Vanhoutte, gaf me een dieper inzicht in de uitdagingen en kansen van het Internet of Things (IoT). Zijn rijke ervaring hebben ongetwijfeld waardevolle lessen geboden voor het bouwen van industriële IoT-platforms op Azure.
+
+Na deze sessie was het tijd voor een welverdiende pauze, en er was een overvloed aan heerlijke broodjes beschikbaar voor iedereen om van te genieten. Het was een geweldige gelegenheid om te netwerken en te praten met mededeelnemers terwijl we van wat lekkers genoten.
+
+Tot slot, de derde sessie, "Azure Durable Functions 101" met Klaas Lauwers, liet ons zien hoe we de schaalbare en betrouwbare serverless applicaties kunnen bouwen met behulp van Azure Durable Functions. Dit is van onschatbare waarde voor ontwikkelaars en architecten die op zoek zijn naar efficiënte oplossingen voor hun toepassingen.
+
+Al met al is het Campus Cloud Café bij Howest in Kortrijk een onvergetelijke en educatieve ervaring geweest.
+
+
+
