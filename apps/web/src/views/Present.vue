d6ebe75fe319a1cdd0283f24317dc5dee928e478
ther <thor.demeestere@telenet.be>
2023-10-20 09:37:36 +0200

    f

--- apps/web/src/views/Present.vue
+++ apps/web/src/views/Present.vue
@@ -5,7 +5,11 @@
         <TRow><TCol>Victrix pro controller</TCol><TCol><Url href="https://pdp.com/products/ps5-ps4-pc-victrix-pro-bfg-wireless-controller-las-almas-le"/></TCol></TRow>
         <TRow><TCol>Usb c dock</TCol><TCol><Url href="https://www.startech.com/en-be/universal-laptop-docking-stations/dk30ch2depue"/></TCol></TRow>
         <TRow><TCol>Deskboard</TCol><TCol><Url href="https://deskboardbuddy.com/pages/deskboard-buddy?utm_content=111271053987&gadid=461911847402"/></TCol></TRow>
-        https://www.tiktok.com/@covertinstruments/video/7274215696823848238
+        <TRow><TCol>Lockpics</TCol><TCol><Url href="https://covertinstruments.com/collections/newly-added/products/riv-pick"/></TCol></TRow>
+        <TRow><TCol>H1 Pro Ergonomic Office Chair</TCol><TCol><Url href="https://eu.hinomi.co/products/hinomi-h1-pro-ergonomic-office-chair?currency=EUR"/></TCol></TRow>
+        <TRow><TCol>Bagpack</TCol><TCol><Url href="https://www.tomtoc.com/products/vintpack-ta1-22l-laptop-backpack"/></TCol></TRow>
+        <TRow><TCol>Robot</TCol><TCol><Url href="https://www.digitaldreamlabs.com/products/vector-robot"/></TCol></TRow>
+        <TRow><TCol>Robot</TCol><TCol><Url href="https://www.digitaldreamlabs.com/products/cozmo-robot"/></TCol></TRow>
     </TableGrid>
 </template>
 
