6b4af3b9228eceff5f6a21af7244bf3ea4b77fa8
Ther <thor.demeestere@telenet.be>
2023-10-22 12:40:50 +0200

    f

--- apps/web/src/components/Layout/Pages/blog.vue
+++ apps/web/src/components/Layout/Pages/blog.vue
@@ -7,27 +7,27 @@
     
 </script>
 
-<style>
-/* Style the main heading */
+<style scoped>
+
 h1 {
-  font-size: 1.75rem; /* Adjust the font size as needed */
+  font-size: 1.75rem; 
   font-weight: bold;
   text-align: center;
-  margin: 1.25rem 0; /* Adjust the margin as needed */
-  color: #333; /* Change this to your desired text color */
+  margin: 1.25rem 0; 
+  color: #333; 
 }
 
-/* Style the subheadings (h2 and h3) */
+
 h2, h3 {
-  font-size: 1.5rem; /* Adjust the font size as needed */
+  font-size: 1.5rem; 
   font-weight: bold;
-  margin: 1.25rem 0; /* Adjust the margin as needed */
-  color: #666; /* Change this to your desired text color */
+  margin: 1.25rem 0; 
+  color: #666; 
 }
 
-/* Style the subheadings (h2) with a different color */
+
 h2 {
-  color: #007bff; /* Change this to your desired text color */
+  color: #007bff; 
 }
 
 </style>
