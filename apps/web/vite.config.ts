0d324b09a7ceb477bfc4c9123ebbb6ba52a58bbf
Ther <thor.demeestere@telenet.be>
2023-10-22 18:24:02 +0200

    f

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,7 +4,7 @@ import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/',
+    base: '/portfolio/',
     server: {
         port: 3000,
         host: '0.0.0.0',
