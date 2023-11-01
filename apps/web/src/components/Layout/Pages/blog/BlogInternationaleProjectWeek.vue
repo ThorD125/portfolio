f2a9df084a8c916b7c8836adcdeb5bb93ff5ffa5
ther <thor.demeestere@telenet.be>
2023-11-01 01:30:29 +0100

    f

--- apps/web/src/components/Layout/Pages/blog/BlogInternationaleProjectWeek.vue
+++ apps/web/src/components/Layout/Pages/blog/BlogInternationaleProjectWeek.vue
@@ -1,11 +1,94 @@
 <template>
+    <div>
+        <h1>Introduction</h1>
 
+        <p>
+            In a globalized world, collaboration and innovation know no boundaries. This past week, a diverse group of
+            talented individuals from around the world converged in the vibrant city of Barcelona, Spain, to embark on
+            an exciting international project. The mission was to create a mobile app using MIT App Inventor, a powerful
+            tool for app development that empowers everyone, regardless of their technical background, to build their
+            own applications.
+        </p>
+
+        <h2>Day 1: Meet and Greet</h2>
+
+        <p>
+            The week kicked off with a lively meet and greet session. Participants hailed from various corners of the
+            globe, each bringing their unique perspectives and skills to the table. In the beautiful backdrop of
+            Barcelona, participants shared their backgrounds, experiences, and what inspired them to join this project.
+            It was a diverse group with a shared goal: to harness the potential of MIT App Inventor to create innovative
+            mobile applications.
+        </p>
+
+        <h2>Day 2: Training and Workshops</h2>
+
+        <p>
+            The second day was all about getting to grips with MIT App Inventor. A series of hands-on workshops and
+            training sessions were conducted, led by experts in mobile app development. Even those with little to no
+            prior experience in coding found themselves quickly understanding the basics and exploring the possibilities
+            of the platform. By the end of the day, participants had their first taste of app development, creating
+            simple apps and discovering the tool's user-friendly interface.
+        </p>
+
+        <h2>Day 3: Brainstorming and Ideation</h2>
+
+        <p>
+            With the fundamentals in place, day three was dedicated to ideation and brainstorming. Teams formed based on
+            complementary skills and interests, and everyone came together to discuss potential app ideas. The goal was
+            to identify real-world problems that could be solved through innovative mobile applications. By the end of
+            the day, each team had a clear vision of their app concept and the problem they aimed to address.
+        </p>
+
+        <h2>Day 4: Prototype and Development</h2>
+
+        <p>
+            The excitement was palpable as participants dove headfirst into app development. Guided by seasoned mentors
+            and instructors, teams started prototyping their apps. MIT App Inventor's drag-and-drop interface made the
+            development process accessible to all, allowing for the rapid creation of functional prototypes. Teams began
+            to see their ideas take shape, as their apps moved closer to reality.
+        </p>
+
+        <h2>Day 5: Testing and Refinement</h2>
+
+        <p>
+            With prototypes in hand, day five was dedicated to rigorous testing and refinement. Participants exchanged
+            feedback and put their apps through their paces, identifying and ironing out bugs and improving user
+            interfaces. The collaborative atmosphere was electric, as everyone helped each other refine their creations,
+            ensuring that the apps would be both user-friendly and effective.
+        </p>
+
+        <h2>Day 6: Final Touches</h2>
+
+        <p>
+            As the week neared its end, teams polished their apps and added the final touches. Graphics, animations, and
+            additional features were integrated to make the apps visually appealing and fully functional. The
+            transformation from app concepts to real, working applications was nothing short of remarkable, showcasing
+            the power of MIT App Inventor in enabling rapid app development.
+        </p>
+
+        <h2>Day 7: Presentation and Celebration</h2>
+
+        <p>
+            The grand finale of the project week arrived on day seven. Each team had the opportunity to present their
+            app to the entire group. The diversity of ideas and solutions was astonishing, ranging from healthcare and
+            education to entertainment and social impact. Celebrations followed as participants applauded each other's
+            efforts, appreciating the immense progress made in just one short week.
+        </p>
+
+        <h2>Conclusion</h2>
+
+        <p>
+            The week in Barcelona, Spain, dedicated to creating apps with MIT App Inventor, exemplified the magic that
+            can happen when diverse minds come together for a common goal. The international project showcased how
+            technology transcends borders and can be a powerful tool for change and innovation. With the shared
+            experiences and knowledge gained during this week, the participants left inspired and empowered to continue
+            their journey in the world of app development. As they return to their respective countries, the impact of
+            this week will undoubtedly be felt in the form of innovative, user-friendly mobile apps that have the
+            potential to make a difference in the lives of people around the world.
+        </p>
+    </div>
 </template>
 
-<script setup lang="ts">
-    
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
