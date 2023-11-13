f4fdb739405ea945c162f77a006efc3155ed750f
ther <thor.demeestere@telenet.be>
2023-11-13 13:10:31 +0100

    f

--- apps/web/src/components/Layout/Pages/cv.vue
+++ apps/web/src/components/Layout/Pages/cv.vue
@@ -1,33 +1,33 @@
 <template>
     <div>
-        <h2 class="text-2xl font-bold">Experiences</h2>
+        <h2>Experiences</h2>
         <div>
-            <h2 class="text-xl font-semibold">Telivic</h2>
+            <h2>Telivic</h2>
             <p>juli-augustus 2022</p>
             <p>Ze hadden data van klanten in een database systeem die al 3 of vier keer werd samengevoegt met andere verkregen data, waardoor deze er soms 4 keer in zaten onder een andere naam of met oudere emails, mijn taak was dan om deze op te ruimen, hiernaast werkte ik ook nog aan hun crm om kleine dingen aan te passen om voor hen het proces van opdrachten of invoices sneller en gemakkelijker in te kunnen geven</p>
         </div>
         <div>
-            <h2 class="text-xl font-semibold">Otomaties - Tom Broucke</h2>
+            <h2>Otomaties - Tom Broucke</h2>
             <p>maart 2021</p>
             <p>https://tombroucke.be/</p>
             <p>Mijn opdrachten bestonden uit het ontwerpen van websites en deze in code omzetten en wordpress erop te integreren.</p>
         </div>
     </div>
-    <div class="mt-3">
-        <h2 class="text-2xl font-bold">Education</h2>
+    <div>
+        <h2>Education</h2>
         <div>
-            <h2 class="text-xl font-semibold mt-1">Bachelor toegepaste informatica - Cyber Security Professional</h2>
+            <h2>Bachelor toegepaste informatica - Cyber Security Professional</h2>
             <p>2021-...</p>
             <p>Howest Brugge</p>
             <p>Opleiding tot een innoverende en veelzijdige informaticus met specialisatie full stack-web ontwikkeling, secure software development, web & mobile native apps, blockchain architectuur en development, cybercrime, web- en netwerk pentesten, forensische analyse, malware analyse en reverse engineering. Kennismaking met Big Data, IoT en IT-wetgeving (GDPR). Kortom een stevige basis IT. Eerste jaar hiervan afgewerkt. In september start ik mijn tweede jaar met als keuzetraject Cyber Security Professional: waar ik vooral cybersecurity voor zal organiseren en leren afrekenen met cybercrime. Geslaagd voor het eerste opleidingsjaar.</p>
         </div>
         <div>
-            <h2 class="text-xl font-semibold mt-1">Tevens behaalde ik ook een stiph2eid, orde, discipline, attitude-attest</h2>
+            <h2>Tevens behaalde ik ook een stiph2eid, orde, discipline, attitude-attest</h2>
             SODA-attest behaald op 29 juni 2021
             https://sodaplus.be/nl-be
         </div>
         <div>
-            <h2 class="text-xl font-semibold mt-1">Multimedia - Optie Visueel</h2>
+            <h2>Multimedia - Optie Visueel</h2>
             <p>2015-2021</p>
             <p>Athena Heule</p>
             <p>https://www.ah2ena-school.be/onze-school/campus-heule</p>
@@ -35,14 +35,14 @@
             Diploma behaald op 29 juni 2021
         </div>
         <div>
-            <h2 class="text-xl font-semibold mt-1">Industriele Wetenschappen</h2>
+            <h2>Industriele Wetenschappen</h2>
             <p>2012-2015</p>
             <p>Vti Menen</p>
             <p>Hier zagen we veel wiskunde.</p>
         </div>
     </div>
-    <div class="mt-3">
-        <h2 class="text-2xl font-bold">Skills</h2>
+    <div>
+        <h2>Skills</h2>
         <div>
             <p>python</p>
         </div>
@@ -65,37 +65,37 @@
             <p>kotlin</p>
         </div>
     </div>
-    <div class="mt-3">
-        <h2 class="text-2xl font-bold">plus points</h2>
+    <div>
+        <h2>plus points</h2>
         <div>
-            <p class="text-xl font-semibold mt-1">Gedreven werker</p>
+            <p>Gedreven werker</p>
             <p>Ik werk liever s'avonds een uurtje verder dan een nieuwe functie of iets dergelijks te laten liggen tot de dag erna.</p>
         </div>
         <div>
-            <p class="text-xl font-semibold mt-1">Leergierig</p>
+            <p>Leergierig</p>
             <p>Ik leer graag nieuwe dingen of leer graag betere manieren kennen voor iets te doen.</p>
         </div>
         <div>
-            <p class="text-xl font-semibold mt-1">Relaxed</p>
+            <p>Relaxed</p>
             <p>Als ik zeg dat iets af zal zijn zal ik zo veel mogelijk doen zodat dit als er geen externe factoren dit tegenhouden dit ook af zal zijn en dat u op 2 geruste oren kunst slapen</p>
         </div>
     </div>
-    <div class="mt-3">
-        <h2 class="text-2xl font-bold">min points</h2>
+    <div>
+        <h2>min points</h2>
         <div>
-            <p class="text-xl font-semibold mt-1">Dislectisch</p>
+            <p>Dislectisch</p>
             <p>Ik doe mijn best maar er zitten waarschijnlijk veel schrijffouten in mijn teksten.</p>
         </div>
         <div>
-            <p class="text-xl font-semibold mt-1">Niet detail oriented</p>
+            <p>Niet detail oriented</p>
             <p>Ik werk liever snel een prototype af waarbij alles werkt, dan dat het er supergoed uitziet of dat het de meest geoptimaliseerde code is.</p>
         </div>
         <div>
-            <p class="text-xl font-semibold mt-1">Solitair</p>
+            <p>Solitair</p>
             <p>Ik geef er de voorkeur aan om zelfstandig te werken in plaats van in een groep.</p>
         </div>
         <div>
-            <p class="text-xl font-semibold mt-1">Sociale interactie</p>
+            <p>Sociale interactie</p>
             <p>Het kost me enige tijd om volledig mijn ware zelf te tonen aan nieuwe kennissen of collega's.</p>
         </div>
     </div>
