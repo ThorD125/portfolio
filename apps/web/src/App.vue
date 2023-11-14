c48f0b874ffb8e6e00adf401016027b4d2e594bf
ther <winlab@thor-demeestere.be>
2023-11-14 17:04:14 +0100

    f

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,22 +1,15 @@
 <template>
     <div class="p-5 md:px-20 xl:px-40 h-screen w-screen flex flex-wrap overflow-hidden">
         <div class="navbar flex justify-end w-full h-min max-h-[10%]">
-        <!-- <TabNav tab="Javascript" :currentTab="currentTab" @actualsettab="actualsettab" /> -->
-        <!-- <TabNav tab="About me" :currentTab="currentTab" @actualsettab="actualsettab" />
-        <TabNav tab="CV" :currentTab="currentTab" @actualsettab="actualsettab" />
-        <TabNav tab="Python" :currentTab="currentTab" @actualsettab="actualsettab" /> -->
-        <!-- <TabNav tab="Bash" :currentTab="currentTab" @actualsettab="actualsettab" /> -->
-        <!-- <TabNav tab="Projects" :currentTab="currentTab" @actualsettab="actualsettab" :default="true" /> -->
-        <!-- <TabNav tab="Blog" :currentTab="currentTab" @actualsettab="actualsettab" /> -->
-        <!-- <TabNav tab="test" :currentTab="currentTab" @actualsettab="actualsettab" /> -->
         <!-- 
-        <Spacer></Spacer>
+            <Spacer></Spacer>
             <div class="w-full">
-            <Color color="#1e90ff" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
-            <Color color="#8a2be2" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
-            <Color color="#ff00ff" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor" :selected="true"></Color>
-            <Color color="red" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
-        </div> -->
+                <Color color="#1e90ff" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
+                <Color color="#8a2be2" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
+                <Color color="#ff00ff" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor" :selected="true"></Color>
+                <Color color="red" :currentcolor="currentColor" @actualsetcolor="actualsetcurrentcolor"></Color>
+            </div> 
+        -->
         <TabNav to="/">About me</TabNav>
         <TabNav to="/CV">CV</TabNav>
         <TabNav to="/blog">Blog</TabNav>
@@ -28,7 +21,7 @@
             <RouterView :key="$route.fullPath" />
         </div>
     </div>
-    <footer class="w-full absolute bottom-0 flex justify-center items-center text-center h-[5vh]">Thor Demeestere · Powered by vue</footer>
+    <footer class="w-full absolute bottom-0 flex justify-center items-center text-center h-[5vh]">© Thor Demeestere · Powered by vue</footer>
 </template>
 <script setup lang="ts"></script>
 
