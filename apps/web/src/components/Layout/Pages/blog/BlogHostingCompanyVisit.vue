029e71c0fa422aef17f4bde38271e8fb18ccc17d
ther <thor.demeestere@telenet.be>
2024-02-10 00:02:52 +0100

    f

---
+++ apps/web/src/components/Layout/Pages/blog/BlogHostingCompanyVisit.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>
+        During our visit to the hosting company DCStar, we were guided through various server areas, each meticulously
+        organized to optimize performance and security. The tour included an in-depth explanation of the innovative
+        ventilation system, designed to maintain optimal temperatures for the servers, ensuring their efficiency and
+        longevity. We also delved into the intricacies of their power delivery system, which is engineered for
+        redundancy to guarantee uninterrupted service, highlighting DCStar's commitment to reliability and
+        sustainability in their operations. This visit not only showcased the advanced technological infrastructure but
+        also the company's dedication to providing a resilient and eco-friendly hosting environment.
+    </p>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
