4da5a1cdb81b182010778d38b8c08a1919dbe0a8
ther <thor.demeestere@telenet.be>
2024-02-09 13:30:10 +0100

    f

--- apps/web/src/views/projects.vue
+++ apps/web/src/views/projects.vue
@@ -1,33 +1,37 @@
 <template>
     <div class="w-full h-full flex flex-wrap justify-between">
-        <ProjectTile>
-            <Url slot="title" href="ksfe" title="sklkflj" />
-            <p class="text-sm">adlksffdkljjl</p>
-            <ProjectLanguage type="java"/>
+        <!-- {{ projectlist }} -->
+        <ProjectTile v-for="project in projectlist">
+            <Url slot="title" :href="project.svn_url" :title="project.name" />
+            <p class="text-sm">{{ project.description }}</p>
+            <ProjectLanguage :type="project.language" />
         </ProjectTile>
-        <ProjectTile>
-            <h2 class="m-0"><Url slot="title" href="ksfe" title="sklkflj" /></h2>
-            <p class="text-sm">adlksffdkljjl</p>
-            <ProjectLanguage type="java"/>
-        </ProjectTile>
-        <ProjectTile>
-            <Url slot="title" href="ksfe" title="sklkflj" />
-            <p class="text-sm">adlksffdkljjl</p>
-            <ProjectLanguage type="java"/>
-        </ProjectTile>
-        <ProjectTile>
-            <Url slot="title" href="ksfe" title="sklkflj" />
-            <p class="text-sm">adlksffdkljjl</p>
-            <ProjectLanguage type="java"/>
-        </ProjectTile>
-
     </div>
 </template>
 
 <script setup lang="ts">
-    
+    import { ref } from 'vue';
+
+    let projectlist = ref<any>();
+
+    const getProjects = async () => {
+        fetch('https://api.github.com/users/ThorD125/repos')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                var sorted = data.sort((a, b) => {
+                    const dateA = new Date(a.updated_at).getTime();
+                    const dateB = new Date(b.updated_at).getTime();
+                    const result = dateA < dateB;
+                    // console.log(result);
+                    return result ? -1 : 1;
+                });
+                console.log(sorted);
+                projectlist.value = sorted;
+            })
+    };
+
+    getProjects();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
