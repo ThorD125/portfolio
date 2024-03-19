2fad1c9b99e3120e900775c10846ef35ec46893f
ther <thor.demeestere@telenet.be>
2024-03-19 19:50:17 +0100

    workingpdf

--- apps/web/src/components/CvToPdf.vue
+++ apps/web/src/components/CvToPdf.vue
@@ -1,9 +1,26 @@
 <template>
-            <button @click="exportToPDF">Export to PDF</button>
+    <button @click="generatePDF">make PDF</button>
+    <div id="pdf">
+        <div class="p-4">
+            <cv></cv>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
+    import cv from '@/views/cv.vue';
+    import html2pdf from 'html2pdf.js';
 
+    function generatePDF() {
+        const doc = document.querySelector('#pdf');
+        html2pdf(doc, {
+            margin: 10,
+            filename: 'cv.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+        });
+    }
 </script>
 
 <style scoped></style>
